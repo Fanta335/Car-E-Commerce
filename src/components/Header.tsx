88f4036e63827ab96b4bbf0b8b10712a95e184e7
Fanta335 <fantayusuke5210@gmail.com>
2021-10-01 18:28:49 +0900

    code linkの追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,9 +7,14 @@ import './Header.css';
 const Header: VFC = () => (
   <div>
     <Box sx={{ p: 2, color: '#fff', backgroundColor: '#181818' }}>
-      <Grid container alignItems="center">
+      <Grid container alignItems="center" justifyContent="space-between">
         <Grid item>
-          <h1>Car Ecommerse</h1>
+          <h1 className='title'>Car Ecommerse</h1>
+        </Grid>
+        <Grid item>
+          <a href='https://github.com/Fanta335/Car-E-Commerce'>
+            <h2 className='code-link'>CODE</h2>
+          </a>
         </Grid>
       </Grid>
     </Box>
